418e0d42736112cbc7a46c955e8eb9ad8bc35cf0
tamai-hideyuki <hide27yuki@gmail.com>
2026-04-11 08:19:17 +0900

    Renderer を作って DOM に追加

--- src/main.ts
+++ src/main.ts
@@ -11,3 +11,9 @@ const camera = new THREE.PerspectiveCamera(
 )
 camera.position.z = 3
 
+const renderer = new THREE.WebGLRenderer({ antialis: true })
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+app.appendChild(renderer.domElement)
