1958f8bdd979e9be73d1eb8e0bf7db7801ab07f9
tamai-hideyuki <hide27yuki@gmail.com>
2026-04-11 08:12:33 +0900

    Cameraを作る

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,13 @@
 import './style.css'
 import * as THREE from 'three'
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
+camera.position.z = 3
+
