fb74fed5c2bd78c733bdc86eb3f6abf939de7a2a
tamai-hideyuki <hide27yuki@gmail.com>
2026-04-11 09:08:51 +0900

    球体を呼吸させる

--- src/main.ts
+++ src/main.ts
@@ -19,8 +19,13 @@ renderer.setPixelRatio(window.devicePixelRatio)
 const app = document.querySelector<HTMLDivElement>('#app')!
 app.appendChild(renderer.domElement)
 
+const clock = new THREE.Clock()
 renderer.setAnimationLoop(() => {
-  renderer.render(scene, camera)
+    const t = clock.getElapsedTime();
+    const scale = 1 + Math.sin(t * 1.0) * 0.15
+    sphere.scale.setScalar(scale)
+
+    renderer.render(scene, camera)
 })
 
 const geometry = new THREE.SphereGeometry(1, 64, 64)
